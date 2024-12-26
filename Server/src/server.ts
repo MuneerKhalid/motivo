12f5f9d4c2da27e8c543edf8c954bf5f6316a6f5
MuneerKhalid <muneer.ansari.2646@gmail.com>
2024-12-26 19:21:20 +0500

    new

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -13,8 +13,6 @@ connectDB();
 
 const app: Application = express();
 
-
-
 app.use(cors({
   origin: ['https://motivo-c7c3c.firebaseapp.com', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
@@ -30,8 +28,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/task', protect, taskRoutes);
 app.use('/api/category', protect, categoryRoute);
 app.use('/api/user', protect, userRoutes);
-
-
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
